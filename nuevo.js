af556575d59d1cb38744150f90db5d055d6ba90a
diego <diegopenavvzz@gmail.com>
2023-06-12 12:33:26 -0400

    Aqui va mi NOmbre

---
+++ nuevo.js
@@ -0,0 +1,6 @@
+function sentenceif(anioActual-anioPersona){
+    const = anioActual - anioPersona;
+    if (resultado <10){
+        console.log("la persona tiene menos de 10 años");
+    console.log("")    }
+}
